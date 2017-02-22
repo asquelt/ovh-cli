1dcfb179aa1349b5a1eaae470db6e7a29c8af52e
konrad rzentarzewski <konrad.rzentarzewski@linuxpolska.pl>
2017-02-22 15:44:56 +0100

    new api keys

--- credentials.go
+++ credentials.go
@@ -3,8 +3,8 @@ package main
 // PUIT in this file your app credentials
 
 const (
-	OVH_APP_KEY_EU    = "R8Ykbxsi35uai76a"
-	OVH_APP_SECRET_EU = "0D91yncUjk5G6Jl8rOPzYjjhGPp7zvJh"
+	OVH_APP_KEY_EU    = "sRnRrPEU88TSa8ul"
+	OVH_APP_SECRET_EU = "htTXd26ObNur6wYUH3bSnXl4QYGxaJTS"
 	OVH_APP_KEY_CA    = "djzTOjyF9f9GykEG"
 	OVH_APP_SECRET_CA = "JouMkHKUOzsmqDFNZuqmquCGkaeMPP2x"
 )
